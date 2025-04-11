d3552a850b0d78af43ec162176a5358b8a043277
karFix <109587903+karFix@users.noreply.github.com>
2025-04-11 11:49:17 +0300

    Update uucinsiyetdagilimgrafigi.m

--- uucinsiyetdagilimgrafigi.m
+++ uucinsiyetdagilimgrafigi.m
@@ -1,13 +1,18 @@
 x = [16 7];
+labels = {'Erkek', 'Kadın'};
+percentages = 100*x/ sum(x);
+labelStrings = strcat(labels, {' - '}, string(round(percentages,1)), {'%'});
 plottype = 'pie3';
 
 switch plottype
     case 'bar'
-            bar(x)
-            title('Bar Graph')
+        bar(x)
+        title('Bar Graph')
+
     case {'pie','pie3'}
-        pie3(x)
-        title('Uşak Üniversitesi Kimya Mühendisliği Bölümü Cinsiyet Dağılımı Grafiği')
+        pie3(x, labelStrings)
+        title(sprintf('Uşak Üniversitesi Kimya Mühendisliği Bölümü\nCinsiyet Dağılımı Grafiği'))
+
     otherwise
         warning('Beklenmedik hata oluştu.')
 end
